111faa7214f5185db4f35c2410de4547680e9167
Tom Fox <kot3854@yandex.ru>
2024-04-16 00:07:54 +0300

    initial commit

---
+++ handlers.py
@@ -0,0 +1,6 @@
+from aiogram import Router, types, F
+from aiogram.types import Message
+from aiogram.filters import Command
+
+router = Router()
+
---
+++ utils.py
@@ -0,0 +1,7 @@
+import yaml
+
+def GetBotToken():
+    with open('config.yaml', 'r') as file:
+        data = yaml.safe_load(file)
+
+    return data["BotToken"]
